1129133b166c6adbc72c0a8ff1952b6d583b6e35
Golde Tischler <golde.tischler@gmail.com>
2023-03-02 22:00:47 +0100

    database created

---
+++ freelance-code/server/database.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+
+const fs = require("fs"); 
+
+
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const con = mysql.createConnection({
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS,
+  database: DB_NAME || "freelance_code",
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  let sql = fs.readFileSync(__dirname + "/init_db.sql").toString(); 
+  con.query(sql, function(err, result) { 
+    if (err) throw err;
+    console.log("Table creation 'user_table' was successful!"); 
+
+    console.log("Closing...");
+  });
+
+  con.end();
+});
---
+++ freelance-code/server/init_db.sql
@@ -0,0 +1,36 @@
+
+
+DROP TABLE IF EXISTS user_table;
+CREATE TABLE user_table (
+user_id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(255) UNIQUE NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) UNIQUE NOT NULL,
+avatar VARCHAR(255),
+firstname VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS services;
+CREATE TABLE services (
+service_id INT PRIMARY KEY AUTO_INCREMENT,
+service_type VARCHAR(255) NOT NULL,
+description TEXT,
+languages VARCHAR(255),
+hourly_rate DECIMAL(10, 2),
+resume VARCHAR(255),
+images TEXT,
+user_id INT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES user_table(user_id)
+);
+
+-- inital data for user_table
+INSERT INTO user_table (username, password, email, avatar, firstname)
+VALUES ('johndoe', 'password123', 'johndoe@example.com', 'avatar.png', 'John'),
+       ('janedoe', 'password456', 'janedoe@example.com', NULL, 'Jane'),
+       ('bobsmith', 'password789', 'bobsmith@example.com', 'bob_avatar.png', 'Bob');
+
+-- inital data for services
+INSERT INTO services (service_type, description, languages, hourly_rate, resume, images, user_id)
+VALUES ('Web Design', 'I create beautiful, responsive websites', 'HTML, CSS, JavaScript', 75.00, 'resume.pdf', NULL, 1),
+       ('Graphic Design', 'I design logos, flyers, and more', 'Photoshop, Illustrator', 50.00, NULL, 'design1.jpg, design2.jpg', 2),
+       ('Translation', 'I translate documents from Spanish to English', 'Spanish, English', 25.00, 'resume.pdf', NULL, 3);
