30f4e8dbcc0e83003239d902a5b26c4e6ed2c43b
Golde Tischler <golde.tischler@gmail.com>
2023-03-07 20:15:16 +0100

    Tailwind config- golde

--- freelance-code/tailwind.config.js
+++ freelance-code/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    'apps/site/pages/**/*.{js,ts,jsx,tsx}',
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
