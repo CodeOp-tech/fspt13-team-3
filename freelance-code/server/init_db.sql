64317bf6bdbab3d91453fc51ca9138df153832cb
Golde Tischler <golde.tischler@gmail.com>
2023-03-08 15:32:00 +0100

    updated database and dashboard to include all info

--- freelance-code/server/init_db.sql
+++ freelance-code/server/init_db.sql
@@ -9,7 +9,7 @@ email VARCHAR(255) UNIQUE NOT NULL,
 avatar VARCHAR(255),
 location VARCHAR(255), 
 firstname VARCHAR(255) NOT NULL,
-lastname VARCHAR(255)
+lastname VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE services (
@@ -20,7 +20,10 @@ skills VARCHAR(255),
 languages VARCHAR(255),
 hourly_rate DECIMAL(10, 2),
 resume VARCHAR(255),
-images TEXT,
+github_url VARCHAR(255),
+linkedin_url VARCHAR(255),
+other_url VARCHAR(255),
+images VARCHAR(255),
 user_id INT NOT NULL,
 FOREIGN KEY (user_id) REFERENCES user_table(user_id) ON DELETE CASCADE
 );
@@ -32,8 +35,7 @@ VALUES ('johndoe', 'password123', 'johndoe@example.com', 'https://cdn.pixabay.co
        ('bobsmith', 'password789', 'bobsmith@example.com', 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png', 'Barcelona, Spain', 'Bob', 'Villanueva');
 
 -- inital data for services
-INSERT INTO services (service_type, skills, description, languages, hourly_rate, resume, images, user_id)
-VALUES ('Web Design', 'communication, frontend', 'I create beautiful, responsive websites', 'HTML, CSS, JavaScript', 75.00, 'resume.pdf', NULL, 1),
-       ('Graphic Design',
-        'research, marketing', 'I design logos, flyers, and more', 'Photoshop, Illustrator', 50.00, NULL, 'design1.jpg, design2.jpg', 2),
-       ('Back end', 'research, data science', 'I design databases', 'Spanish, English', 25.00, 'resume.pdf', NULL, 3);
+INSERT INTO services (service_type, skills, description, languages, hourly_rate, resume, github_url, linkedin_url, other_url, images, user_id)
+VALUES ('Web Design', 'communication, frontend', 'I create beautiful, responsive websites', 'HTML, CSS, JavaScript', 75.00, 'resume.pdf','github.com/johndoe', 'linkedin.com/in/johndoe', 'johndoeportfolio.com', NULL, 1),
+       ('Graphic Design', 'research, marketing', 'I design logos, flyers, and more', 'Photoshop, Illustrator', 50.00, NULL,'github.com/janedoe', 'linkedin.com/in/janedoe', 'janedoeportfolio.com', 'design1.jpg, design2.jpg', 2),
+       ('Back end', 'research, data science', 'I design databases', 'Spanish, English', 25.00, 'resume.pdf', 'github.com/bobsmith', 'linkedin.com/in/bobsmith', 'bobsmithportfolio.com', NULL, 3);
