ba58ae05e81f9b81568fbb4d342587399438ae18
Golde Tischler <golde.tischler@gmail.com>
2023-03-09 14:56:00 +0100

    dashboard styled, delete func, & remove token

--- freelance-code/pages/dashboard.js
+++ freelance-code/pages/dashboard.js
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react";
 const DashboardPage = () => {
   const [user, setUser] = useState(null);
 
+  const removeToken = () => {
+    localStorage.removeItem('token');
+  }
+
   useEffect(() => {
     const getUser = async () => {
       const token = localStorage.getItem("token");
@@ -39,38 +43,100 @@ const DashboardPage = () => {
     return <div>Loading...</div>;
   }
 
+
+  const deleteUser = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/users/userdetail/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+
+      console.log(response)
+      removeToken();
+    } catch (error) {
+      setError("Oops! Something went wrong. Try again later");
+    }
+  };
+
   return (
-    <section className="dashboard-container">
-      <h1>Welcome, {user.username}!</h1>
+    <div className="w-full max-w-3xl mx-auto">
+      <p>dashboard</p>
 
-      <div className="name-location-hourlyrate">
-        {/* change to image tag later  */}
-        <div>{user.avatar}</div>
-        <h1>
-          {user.firstname} {user.lastname ? <h1>{user.lastname}</h1> : null}
-        </h1>
-        <p>{user.locaton}</p>
-        <p>{user.hourly_rate}/hour</p>
+      <div className="flex flex-col border border-gray-400 rounded-lg p-5 bg-white">
+        <div className="flex flex-col gap-3 justify-between mb-4 sm:flex-row">
+          <div className="flex flex-col items-center sm:flex-row">
+            <img
+              className="w-32 h-32 rounded-full"
+              src={user.avatar}
+              alt={`profile image of ${user.firstname}`}
+            />
+            <div className="ml-2">
+              <p className="font-medium text-lg text-coBlue">
+                {user.firstname} {user.lastname}
+              </p>
+              <p className="text-sm">{user.location}</p>
+            </div>
+          </div>
+          <div>
+            <div className="bg-coYellow py-1 px-4 font-bold">
+              {user.hourly_rate} €/hr
+            </div>
+          </div>
+        </div>
+        <div className="mb-4">
+          <h3 className="font-bold text-xl">{user.service_type}</h3>
+          <p>{user.description}</p>
+        </div>
+        <div className="mb-4">
+          <h4 className="font-bold text-lg">Portfolio</h4>
+          <hr className="mb-2"></hr>
+          <div className="flex flex-col gap-1 justify-between sm:flex-row">
+            <div>
+              <p>{user.github_url}</p>
+              <p>{user.linkedin_url}</p>
+              <p>{user.other_url}</p>
+            </div>
+            <div>
+              <p>{user.resume}</p>
+            </div>
+          </div>
+        </div>
+        <div className="mb-4">
+          <h4 className="font-bold text-lg">Skills</h4>
+          <hr className="mb-2"></hr>
+         {/*  {user.skills.split(",").map((skill, index) => {
+            return (
+              <span
+                key={index}
+                className="inline-block bg-coGrey rounded-full px-3 py-0.5 text-xs text-gray-900 mr-2 mb-2"
+              >
+                {skill}
+              </span>
+            );
+          })} */}
+        </div>
+        <div className="mb-4">
+          <h4 className="font-bold text-lg">Languages</h4>
+          <hr className="mb-2"></hr>
+          <p>{user.languages}</p>
+        </div>
+        <div className="mb-4">
+          <h4 className="font-bold text-lg">Contact Info</h4>
+          <hr className="mb-2"></hr>
+          <p>{user.email}</p>
+        </div>
+        <div className="flex justify-center gap-3">
+          <button className="bg-coGreen hover:bg-emerald-500 text-white py-1 px-4 rounded-md">
+            Edit
+          </button>
+          <button  onClick={() => deleteUser(user.user_id)} className=" bg-red-600 hover:bg-red-800 text-white py-1 px-4 rounded-md">
+            Delete
+          </button>
+        </div>
       </div>
-
-      <div className="service-info">
-        <h1>{user.service_type}</h1>
-        <p>{user.description}</p>
-      </div>
-
-      <div className="portfolio-section"></div>
-      <p>{user.resume}</p>
-      <p>{user.github_url}</p>
-      <p>{user.linkedin_url}</p>
-      <p>{user.other_url}</p>
-      
-      <p>{user.skills}</p>
-      <p>{user.languages}</p>
-      <p>Contact: {user.email}</p>
-
-      <buton>Edit</buton>
-      <button>Delete Profile</button>
-    </section>
+    </div>
   );
 };
 export default withAuth(DashboardPage);
