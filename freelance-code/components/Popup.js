683e8cd2b6cceb7c13582475191fd48349e48a4a
Golde Tischler <golde.tischler@gmail.com>
2023-03-16 12:29:37 +0100

    styling on loading message

--- freelance-code/components/Popup.js
+++ freelance-code/components/Popup.js
@@ -1,6 +1,6 @@
 function Popup({ handleDeleteTrue, handleDeleteFalse }) {
   return (
-    <div className="modal fixed z-10 left-0 top-0 w-full h-full overflow-auto backdrop-brightness-50 backdrop-blur-sm">
+    <div className="modal fixed z-10 left-0 top-0 w-full h-full pt-40 overflow-auto backdrop-brightness-50 backdrop-blur-sm">
       <div className="modal-box bg-white my-1/6 mx-auto p-6 border-solid border-4 border-coGreen rounded-md w-2/4">
         <div className="modal-head flex justify-between">
           <p className="m-0 pt-5 px-0 leading-4 font-semibold text-lg">
