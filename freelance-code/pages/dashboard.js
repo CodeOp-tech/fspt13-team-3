e2ea12c3b06c3ce33d91da91ea9426042d7bbaef
Golde Tischler <golde.tischler@gmail.com>
2023-03-16 16:50:53 +0100

    added success component to dashboard

--- freelance-code/pages/dashboard.js
+++ freelance-code/pages/dashboard.js
@@ -8,6 +8,7 @@ import GeneralLoadingMessage from "@/components/GeneralLoadingMessage";
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { MdEmail, MdLocationOn } from 'react-icons/md';
 import { BsChevronCompactRight } from 'react-icons/bs';
+import SuccessMessage from "@/components/SuccessMessage";
 
 
 const DashboardPage = () => {
@@ -16,6 +17,7 @@ const DashboardPage = () => {
   const [error, setError] = useState(null);
   const { successMessage } = router.query;
   const [successEmpty, setSuccessEmpty] = useState("");
+  
   const [userDeleted, setUserDeleted] = useState(false);
   const [popup, setPopup] = useState({
     show: false,
@@ -150,18 +152,7 @@ const DashboardPage = () => {
 
         <div>
           {successMessage && successEmpty.length > 0 ? (
-            <section className="modal fixed z-10 left-0 top-0 w-full h-full  pt-40 overflow-auto backdrop-brightness-50 backdrop-blur-sm">
-              <div className="modal-box bg-white my-1/6 mx-auto p-6 border-solid border-4 border-coGreen rounded-md w-2/4">
-                <div className="modal-head flex justify-between">
-                  <p className="m-0 pt-5 px-0 leading-4 font-semibold text-lg">
-                    {successEmpty}
-                  </p>
-                  <div className="cancel-icon cursor-pointer h-10 w-10 rounded-full grid place-items-center bg-coGreen">
-                    ✔️
-                  </div>
-                </div>
-              </div>
-            </section>
+            <SuccessMessage successMessage={successMessage} />
           ) : null}
         </div>
 
