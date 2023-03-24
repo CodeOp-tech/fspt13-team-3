46b83f54b4c6dffa10e472c6a4fb01bbc47c075f
Golde Tischler <golde.tischler@gmail.com>
2023-03-25 00:12:42 +0100

    cleanedup: withAuth

--- freelance-code/components/withAuth.js
+++ freelance-code/components/withAuth.js
@@ -1,26 +1,3 @@
-/* import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
-const withAuth = (WrappedComponent) => {
-  const Wrapper = (props) => {
-    const router = useRouter();
-
-    useEffect(() => {
-      const token = localStorage.getItem("token");
-      if (!token) {
-        router.push('/login');
-      }
-    }, []);
-
-    return <WrappedComponent {...props} />;
-  };
-
-  return Wrapper;
-};
-
-export default withAuth;
- */
-
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
